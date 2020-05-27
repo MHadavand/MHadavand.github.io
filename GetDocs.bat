b6b494ac2f5753d9543e7a8be9330bf45f843538
MHadavand <hadavand@ualberta.ca>
2020-05-26 22:17:04 -0600

    Pygeostat doc referesh

--- GetDocs.bat
+++ GetDocs.bat
@@ -1,7 +1,15 @@
 @ECHO OFF
 
+CALL conda activate Geostat
+
+ECHO Building Pygeostat documentation...
+
+CALL ..\pygeostat_public\doc\make.bat clean
+
+CALL ..\pygeostat_public\doc\make.bat html
+
 ECHO Copying Pygeostat documentation...
 
-xcopy /s/e ..\pygeostat_public\doc\build\html .\static\Pygeostat -A
+xcopy /s/e/y ..\pygeostat_public\doc\build\html .\static\Pygeostat
 
 ECHO Done
