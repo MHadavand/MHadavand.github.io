3bcdfc57748395badea7b65ae2f852597882dac2
MHadavand <hadavand@ualberta.ca>
2020-05-26 22:30:32 -0600

    Updated data analysis documentation

--- GetDocs.bat
+++ GetDocs.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
+REM Pygeostat Document
 REM CALL conda activate Geostat
 
 REM ECHO Building Pygeostat documentation...
@@ -8,15 +9,36 @@ REM CALL ..\pygeostat_public\doc\make.bat clean
 
 REM CALL ..\pygeostat_public\doc\make.bat html
 
+REM ECHO Copying Pygeostat documentation...
+
+REM xcopy /s/e/y ..\pygeostat_public\doc\build\html .\static\Pygeostat
+
+REM git checkout master
+
+REM git add .
+
+REM git commit -am "Updated pygeostat documentation"
+
+REM git push
+
+
+REM Data Analysis Documents
+
+ECHO Building Data analysis documentation...
+
+CALL ..\Lessons\DataAnalysis\make.bat clean
+
+CALL ..\Lessons\DataAnalysis\make.bat html
+
 ECHO Copying Pygeostat documentation...
 
-xcopy /s/e/y ..\pygeostat_public\doc\build\html .\static\Pygeostat
+xcopy /s/e/y ..\Lessons\DataAnalysis\build\html .\static\DataAnalysis
 
 git checkout master
 
 git add .
 
-git commit -am "Updated pygeostat documentation"
+git commit -am "Updated data analysis documentation"
 
 git push
 
